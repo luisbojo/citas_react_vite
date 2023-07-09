402b894b3031f65d64b18517a2c8fafa5410c755
luisbojo <138181828+luisbojo@users.noreply.github.com>
2023-07-08 19:55:27 -0600

    Segundo commit de branch"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header(){
     return(
         <h1 className="font-black text-4xl text-center md:w-2/3 mx-auto" >
-            Seguimiento pacientes {""}
+            Seguimiento pacientes {""} Con Github{''}
             <span className="text-indigo-600">Veterinaria</span>
         </h1>
     )
